03b42d83beec58335e7938d4d9509e6344a978ad
Pablo <pabloroblefra@gmail.com>
2026-06-25 20:33:15 -0500

    feat: add MySQL load to load module with dotenv credentials

--- src/load.py
+++ src/load.py
@@ -1,6 +1,9 @@
 import logging
 import pandas as pd
 import os
+import mysql.connector
+from sqlalchemy import create_engine 
+from dotenv import load_dotenv
 
 #Config Logger
 logging.basicConfig(
@@ -23,13 +26,41 @@ def save_csv(df, path, name):
 def load_all(cleaned_df, expanded_skills_df, processed_path):
     logger.info("Starting to load dataframes to CSV files")
     
-    save_csv(cleaned_df, f"{processed_path}/expanded_skills.csv", "Cleaned Jobs DataFrame")
+    save_csv(cleaned_df, f"{processed_path}/cleaned_jobs.csv", "Cleaned Jobs DataFrame")
     save_csv(expanded_skills_df[["job_link", "job_skills"]], f"{processed_path}/expanded_skills.csv", "Expanded Skills DataFrame")
     logger.info("All dataframes loaded successfully to CSV files")
+
+def get_mysql_conection(host, user, password, database):
+    logger.info(f"Connecting to MySQL database: {database} at {host} with user {user}")
+    try:
+        connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        logger.info("MySQL connection established successfully")
+        return connection
+    except mysql.connector.Error as e:
+        logger.error(f"Error connecting to MySQL, Error: {e}")
+        raise
+
+def load_to_mysql(cleaned_df, expanded_skills_df, user, password, host="localhost", database="job_market"):
+    logger.info("Loading dataframes to MySQL database")
+    engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{database}")
+    cleaned_df.to_sql("cleaned_jobs", con=engine, if_exists="replace", index=False)
+    logger.info(f"cleaned_jobs loaded - {len(cleaned_df)} rows")
+    expanded_skills_df[["job_link", "job_skills"]].to_sql("expanded_skills", con=engine, if_exists="replace", index=False)
+    logger.info(f"expanded_skills loaded - {len(expanded_skills_df)} rows")
 
 if __name__ == "__main__":
     from transform import transform_all
     from extract import extract_all
+
+    load_dotenv()  # Load environment variables from .env file
+
+    user = os.getenv("MYSQL_USER")
+    password = os.getenv("MYSQL_PASSWORD")
 
     raw_path = "data/raw"
     processed_path = "data/processed"
@@ -38,6 +69,12 @@ if __name__ == "__main__":
     
     cleaned_df, expanded_skills_df = transform_all(jobs_df, skills_df, summary_df)
     
+
+
     load_all(cleaned_df, expanded_skills_df, processed_path)
+
+    load_to_mysql(cleaned_df, expanded_skills_df, user=user, password=password)
+
+    logger.info("MySQL loading process completed successfully")
 
     logger.info("Data loading process completed successfully")
